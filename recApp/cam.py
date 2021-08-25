425ba70195de97d0203b508043918578e9ba1258
Martin Smith <martynas.smi@gmail.com>
2021-08-25 12:24:06 +0300

    It has been too long to remember

--- recApp/cam.py
+++ recApp/cam.py
@@ -6,11 +6,22 @@ from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 from kivy.properties import ObjectProperty, StringProperty, NumericProperty
+from kivy.config import Config
 
 import cv2
 import os
 
 import datetime
+
+# 0 being off 1 being on as in true / false
+# you can use 0 or 1 && True or False
+Config.set("graphics", "resizable", "0")
+
+# fix the width of the window
+Config.set("graphics", "width", "800")
+
+# fix the height of the window
+Config.set("graphics", "height", "480")
 
 # Standard Video Dimensions Sizes
 STD_DIMENSIONS = {
@@ -23,34 +34,37 @@ STD_DIMENSIONS = {
 # Video Encoding, might require additional installs
 # Types of Codes: http://www.fourcc.org/codecs.php
 VIDEO_TYPE = {
-    'avi': cv2.VideoWriter_fourcc(*'XVID'),
+    "avi": cv2.VideoWriter_fourcc(*"XVID"),
     # 'mp4': cv2.VideoWriter_fourcc(*'H264'),
-    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+    "mp4": cv2.VideoWriter_fourcc(*"XVID"),
 }
 
 
 class KivyCamera(FloatLayout):
     recording = True
     frames_per_second = NumericProperty(10.0)
-    video_resolution = StringProperty('720p')
+    video_resolution = StringProperty("720p")
     frame_box = ObjectProperty()
-    toggle_rec_text = StringProperty('stop')
+    toggle_rec_text = StringProperty("stop")
 
     def __init__(self, **kwargs):
         super(KivyCamera, self).__init__(**kwargs)
         self.filename = self.gen_filename()
 
         self.capture = cv2.VideoCapture(0)
-        self.out = cv2.VideoWriter('videos/' + self.filename, self.get_video_type(
-            self.filename), self.frames_per_second, self.get_dims(self.capture, self.video_resolution))
+        self.out = cv2.VideoWriter(
+            "videos/" + self.filename,
+            self.get_video_type(self.filename),
+            self.frames_per_second,
+            self.get_dims(self.capture, self.video_resolution),
+        )
         Clock.schedule_interval(self.update, 1 / self.frames_per_second)
 
     def update(self, *args):
         ret, frame = self.capture.read()
         self.out.write(frame)
         buf = cv2.flip(frame, 0).tostring()
-        texture = Texture.create(
-            size=(frame.shape[1], frame.shape[0]), colorfmt="bgr")
+        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt="bgr")
         texture.blit_buffer(buf, colorfmt="bgr", bufferfmt="ubyte")
         self.frame_box.texture = texture
         # self.canvas.ask_update()
@@ -63,7 +77,7 @@ class KivyCamera(FloatLayout):
         self.capture.set(4, height)
 
     # grab resolution dimensions and set video capture to it.
-    def get_dims(self, cap, video_resolution='1080p'):
+    def get_dims(self, cap, video_resolution="1080p"):
         width, height = STD_DIMENSIONS["480p"]
         if self.video_resolution in STD_DIMENSIONS:
             width, height = STD_DIMENSIONS[self.video_resolution]
@@ -76,28 +90,33 @@ class KivyCamera(FloatLayout):
         filename, ext = os.path.splitext(filename)
         if ext in VIDEO_TYPE:
             return VIDEO_TYPE[ext]
-        return VIDEO_TYPE['avi']
+        return VIDEO_TYPE["avi"]
 
     def toggle_recording(self):
         if self.recording:
             Clock.unschedule(self.update)
-            self.toggle_rec_text = 'start'
+            self.toggle_rec_text = "start"
             self.out.release()
         else:
             self.filename = self.gen_filename()
-            self.out = cv2.VideoWriter('videos/' + self.filename, self.get_video_type(
-                self.filename), self.frames_per_second, self.get_dims(self.capture, self.video_resolution))
+            self.out = cv2.VideoWriter(
+                "videos/" + self.filename,
+                self.get_video_type(self.filename),
+                self.frames_per_second,
+                self.get_dims(self.capture, self.video_resolution),
+            )
             Clock.schedule_interval(self.update, 1 / self.frames_per_second)
-            self.toggle_rec_text = 'stop'
+            self.toggle_rec_text = "stop"
         self.recording = not self.recording
-    
+
     def gen_filename(self):
-        return datetime.datetime.now().strftime('%m-%d--%H-%M-%S.avi')
+        return datetime.datetime.now().strftime("%m-%d--%H-%M-%S.avi")
+
 
 class CamApp(App):
     def build(self):
         return KivyCamera()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     CamApp().run()
